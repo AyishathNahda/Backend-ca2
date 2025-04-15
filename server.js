3efa4440a1707f8a0526ccbee213e460d68f4ef1
kalvium <kalvium@kalvium.community>
2025-04-15 08:56:53 +0530

    backend ca2 done

---
+++ server.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+const PORT=3000;
+
+app.post("/login",async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email){
+            return res.send(400).json({message:"Email cannot be empty"});
+        }else if(!password){
+            return res.send(400).json({message:"Password cannot be empty"});
+        }else{
+            return res.send(201).json({message:"Login Successful"});
+        }
+    }catch(err){
+        return res.status(500).json({message:"Internal Server Error",err});
+    }
+});
+
+
+
+app.get("/",(req,res)=>{
+    console.log("<h1>Server is running.</h1>");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
